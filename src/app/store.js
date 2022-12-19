f4e2a2c13c6c81ab67448e233d5d84260ef9a675
ivicakartelo <ivicakartelo@users.noreply.github.com>
2022-12-19 19:45:27 +0100

    Replies to replies

--- src/app/store.js
+++ src/app/store.js
@@ -3,13 +3,15 @@ import postsReducer from '../features/posts/postsSlice'
 import usersReducer from '../features/users/usersSlice'
 import commentsReducer from '../features/comments/commentsSlice'
 import repliesReducer from '../features/replies/repliesSlice'
+import replies1Reducer from '../features/replies1/replies1Slice'
 
 export const store = configureStore({
   reducer: {
     posts: postsReducer,
     users: usersReducer,
     comments: commentsReducer,
-    replies: repliesReducer
+    replies: repliesReducer,
+    replies1: replies1Reducer
   }
 })
 console.log(store.getState())
--- src/features/posts/RepliesList.js
+++ src/features/posts/RepliesList.js
@@ -1,4 +1,6 @@
 import { useSelector } from 'react-redux'
+import { AddReply1Form } from './AddReply1Form'
+import { Replies1List } from './Replies1List'
 
 export const RepliesList = ({ commentId }) => {
   const replies = useSelector(state => state.replies)
@@ -8,6 +10,10 @@ export const RepliesList = ({ commentId }) => {
       <h1>{reply.date}</h1>
       <h3>{reply.author}</h3>
       <p>{reply.reply}</p>
+      <div style={{ marginLeft: '50px', paddingBottom: '1rem' }}>
+      <AddReply1Form replyId={reply.replyId} />
+      <Replies1List replyId={reply.replyId} />
+      </div>
       
     </article>
   ))
